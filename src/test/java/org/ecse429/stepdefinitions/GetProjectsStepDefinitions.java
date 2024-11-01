ac18d48fa3a7ca3db792ca1e566fe69aeec1f933
Lucy Zhang <90661219+Lucy-Zh@users.noreply.github.com>
2024-10-31 22:08:45 -0400

    working get projects step defs

--- src/test/java/org/ecse429/stepdefinitions/GetProjectsStepDefinitions.java
+++ src/test/java/org/ecse429/stepdefinitions/GetProjectsStepDefinitions.java
@@ -19,6 +19,7 @@ public class GetProjectsStepDefinitions {
     @Given("the REST API To-Do List Manager application is running")
     public void the_rest_api_to_do_list_manager_application_is_running() {
         RestAssured.baseURI = BASE_URL;
+        response = null;
     }
     @Given("the following projects exist in the system:")
     public void the_following_projects_exist_in_the_system(io.cucumber.datatable.DataTable dataTable) {
@@ -34,28 +35,46 @@ public class GetProjectsStepDefinitions {
                     .statusCode(201);
         }
     }
-/*    @Given("the following tasks exist for each project:")
-    public void the_following_tasks_exist_for_each_project(io.cucumber.datatable.DataTable dataTable) {
-        List<Map<String, String>> tasks = dataTable.asMaps(String.class, String.class);
-        for (Map<String, String> task : tasks) {
-            String projectId = task.get("project_id");
-            String taskId = task.get("task_id");
-            RestAssured.given()
-                    .contentType("application/json")
-                    .body("{\"title\": \"test_task\"}")
-                    .post(BASE_URL + "/" + projectId + "/tasks")
-                    .then()
-                    .statusCode(201);
+    @Given("no project exists in the system")
+    public void no_project_exists_in_the_system() {
+        response = RestAssured.get(BASE_URL);
+        List<String> projectIds = response.jsonPath().getList("projects.id");
+        for (String projectId : projectIds) {
+            RestAssured.delete(BASE_URL + "/" + projectId).then().statusCode(200);
         }
-    }*/
+    }
     @When("user requests to retrieve all projects")
     public void user_requests_to_retrieve_all_projects() {
         response = RestAssured.get(BASE_URL);
+    }
+    @When("user requests to retrieve a specific project with id <project_id>")
+    public void user_requests_to_retrieve_a_specific_project_with_id() {
+        response = RestAssured.get(BASE_URL);
+        response = RestAssured.get(BASE_URL + "/" + response.jsonPath().getList("projects.id").get(0));
+    }
+    @When("user requests to retrieve project with project id NONEXISTENT_PROJECT_ID")
+    public void user_requests_to_retrieve_a_specific_project_with_id_does_not_exist() {
+        response = RestAssured.get(BASE_URL + "/" + 0); // id is never 0 so always nonexistent
     }
     @Then("a list of all projects in the system and its details should be displayed")
     public void a_list_of_all_projects_in_the_system_and_its_details_should_be_displayed() {
         List<Map<String, Object>> projects = response.jsonPath().getList("projects.id");
         assertFalse("Expected list of projects, but got none.", projects.isEmpty());
+    }
+    @Then("an empty list of projects should be displayed")
+    public void an_empty_list_of_projects_should_be_displayed(){
+        List<Map<String, Object>> projects = response.jsonPath().getList("projects.id");
+        assertTrue("Expected list of projects, but got none.", projects.isEmpty());
+    }
+    @Then("the details of project with id <project_id> should be displayed")
+    public void the_details_of_project_with_id_should_be_displayed() {
+        List<Map<String, Object>> projects = response.jsonPath().getList("projects.id");
+        assertFalse("Expected list of projects, but got none.", projects.isEmpty());
+    }
+    @Then("an {string} message is displayed")
+    public void a_specific_message_is_displayed(String message) {
+        String actualError = (String) response.jsonPath().getList("errorMessages").get(0);
+        assertEquals(message, actualError);
     }
     @Then("the response status code should be {int}")
     public void the_response_status_code_should_be(int expectedStatusCode) {
--- src/test/resources/features/ID001_Get_Projects.feature
+++ src/test/resources/features/ID001_Get_Projects.feature
@@ -9,12 +9,45 @@ Given the REST API To-Do List Manager application is running
 
 Scenario: Retrieve all projects (Normal Flow)
 
-Given the following projects exist in the system:
-| project_id | title    | completed | active | description   |
-| 1          | project1 | false     | true   | my comment    |
-| 2          | project2 | true      | false  |               |
-| 3          | project3 | false     | true   | i like this   |
-| 4          | project4 | false     | false  | cool project  |
-When user requests to retrieve all projects
-Then a list of all projects in the system and its details should be displayed
-And the response status code should be 200
+  Given the following projects exist in the system:
+  | project_id | title    | completed | active | description   |
+  | 1          | project1 | false     | true   | my comment    |
+  | 2          | project2 | true      | false  |               |
+  | 3          | project3 | false     | true   | i like this   |
+  | 4          | project4 | false     | false  | cool project  |
+  When user requests to retrieve all projects
+  Then a list of all projects in the system and its details should be displayed
+  And the response status code should be 200
+
+Scenario: Retrieve all projects but there is no project in the system (Alternate Flow)
+
+  Given no project exists in the system
+  When user requests to retrieve all projects
+  Then an empty list of projects should be displayed
+  And the response status code should be 200
+
+
+Scenario: Retrieve a specific project by project id (Alternate Flow)
+
+  Given the following projects exist in the system:
+    | project_id | title    | completed | active | description   |
+    | 1          | project1 | false     | true   | my comment    |
+    | 2          | project2 | true      | false  |               |
+    | 3          | project3 | false     | true   | i like this   |
+    | 4          | project4 | false     | false  | cool project  |
+  When user requests to retrieve a specific project with id <project_id>
+  Then the details of project with id <project_id> should be displayed
+  And the response status code should be 200
+
+
+Scenario: Retrieve a project that does not exist (Error Flow)
+
+  Given the following projects exist in the system:
+    | project_id | title    | completed | active | description   |
+    | 1          | project1 | false     | true   | my comment    |
+    | 2          | project2 | true      | false  |               |
+    | 3          | project3 | false     | true   | i like this   |
+    | 4          | project4 | false     | false  | cool project  |
+  When user requests to retrieve project with project id NONEXISTENT_PROJECT_ID
+  Then an "Could not find an instance with projects/0" message is displayed
+  And the response status code should be 404
